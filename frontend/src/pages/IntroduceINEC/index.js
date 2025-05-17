6677618755dca250efe37ee0e201bb594bc800b4
dangduong0988 <dangduong0127@gmail.com>
2025-05-17 11:51:56 +0700

    update introduce page

--- frontend/src/pages/IntroduceINEC/index.js
+++ frontend/src/pages/IntroduceINEC/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.scss";
-
+import WhatOurClientsSay from "../HomePage/WhatOurClientsSay";
+import TitleBradingLine from "../HomePage/Title&BrandingLine";
 const IntroduceINEC = () => {
   return (
     <div className="Introduce-wrapper">
@@ -118,6 +119,242 @@ const IntroduceINEC = () => {
                       allowFullScreen
                     />
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="thrid-section">
+        <div className="thrid-wrapper">
+          <div className="title">
+            <h2>Những con số ấn tượng về INEC</h2>
+          </div>
+
+          <div className="thrid-container">
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/18-year-anniversary-01-400x400.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>18</strong>
+                <p>Năm hoạt động</p>
+              </div>
+            </div>
+
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/cac-con-so-inec-500.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>500+</strong>
+                <p>Trường đối tác tuyển sinh</p>
+              </div>
+            </div>
+
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/cac-con-so-inec-tuyen-sinh-toan-cau.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>20+</strong>
+                <p>Quốc gia tuyển sinh</p>
+              </div>
+            </div>
+
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/cac-con-so-inec-10000-sv.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>10.000+</strong>
+                <p>Sinh viên du học</p>
+              </div>
+            </div>
+
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/cac-con-so-inec-63-tinh.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>63</strong>
+                <p>Tỉnh thành INEC tuyển sinh</p>
+              </div>
+            </div>
+
+            <div className="thrid-container-item">
+              <div className="thrid-image">
+                <img
+                  src="https://duhocinec.com/wp-content/uploads/2024/11/experiencelife-300x300.png"
+                  alt=""
+                />
+              </div>
+              <div className="thrid-content">
+                <strong>95%</strong>
+                <p>Khách hàng hài lòng</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <WhatOurClientsSay />
+
+      <section className="fourth-section">
+        <div className="container">
+          <TitleBradingLine
+            title="Trụ sở INEC"
+            description="Văn phòng tại TP.Hồ Chí Minh"
+          />
+
+          <div className="fourth-inner">
+            <div className="four-left">
+              <div className="col-left">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/1.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/2.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/3.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-right">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/4.-293x613-hinh-van-phong-hcm-490x1024.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="four-right">
+              <div className="col-left">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/5.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/6.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/7.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-right">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/8.-293x613-hinh-van-phong-hcm-490x1024.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <p>Văn phòng tại Đà Nẵng</p>
+          <div className="fourth-inner">
+            <div className="four-left">
+              <div className="col-left">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/1.-293x185-hinh-van-phong-hcm-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/2.-293x185-hinh-van-phong-dn-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/3.-293x185-hinh-van-phong-dn-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-right">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/4.-293x613-hinh-van-phong-dn-490x1024.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="four-right">
+              <div className="col-left">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/5.-293x185-hinh-van-phong-dn-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/6.-293x185-hinh-van-phong-dn-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/7.-293x185-hinh-van-phong-dn-1024x647.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-right">
+                <div className="col-inner">
+                  <img
+                    src="https://duhocinec.com/wp-content/uploads/2024/11/8.-293x613-hinh-van-phong-dn-490x1024.jpg"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
